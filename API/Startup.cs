6531d59263ddb4873bb1d9220027956ff24adc26
Roman Podlesniy <romanpodlesniy2021@gmail.com>
2021-12-17 18:42:43 +0300

    delate ssl kay

--- API/Startup.cs
+++ API/Startup.cs
@@ -57,7 +57,7 @@ namespace API
             app.UseRouting();
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-if
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
