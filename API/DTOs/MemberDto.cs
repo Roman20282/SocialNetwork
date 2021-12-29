eca2eae3c47a01ff232f08ccafac061b0186705c
Roman Podlesniy <romanpodlesniy2021@gmail.com>
2021-12-29 16:45:21 +0300

    Using AutoMapper for adding Photo in postman

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -5,8 +5,9 @@ namespace API.DTOs
     public class MemberDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string LastName { get; set; }
+        public string PhotoUrl { get; set; }
         public int Age { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
